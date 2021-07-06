89e64ebdb904c4ee95ac82599865bbb3af1aac25
Labiba Ibnat <labiba.ssp@gmail.com>
2021-07-07 01:32:52 +0600

    feat: add navbar

---
+++ components/navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import { useStyles } from "./Style";
+import Image from "next/image";
+import Logo from "../../public/images/mlbdLogo.png";
+
+export default function SearchAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <Image src={Logo} alt="logo" width={20} height={20} />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            MonstarLab Bangladesh
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
