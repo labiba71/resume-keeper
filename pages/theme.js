1180eacbdfbf86ae043a07be7f607d89da1bb70a
Labiba Ibnat <labiba.ssp@gmail.com>
2021-07-07 01:33:42 +0600

    feat: add index page with style and theme

---
+++ pages/theme.js
@@ -0,0 +1,22 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#53575A",
+    },
+    secondary: {
+      main: "#FCFD02",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#fff",
+    },
+  },
+});
+
+export default theme;
